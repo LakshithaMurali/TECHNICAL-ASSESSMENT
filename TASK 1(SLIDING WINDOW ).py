2e24d54852c9eca6ac27089bbe408c5a3c2025bb
Lakshitha M <153210828+LakshithaMurali@users.noreply.github.com>
2023-12-10 14:21:22 +0530

    Update TASK 1(SLIDING WINDOW ).py

--- TASK 1(SLIDING WINDOW ).py
+++ TASK 1(SLIDING WINDOW ).py
@@ -1,20 +1,20 @@
-def findBestValue(food, people):
+def findTotalValue(food, people):
     n = len(food)
     prefix_sum = [0] * (n + 1)
     for i in range(n):
         prefix_sum[i + 1] = prefix_sum[i] + food[i]
     total_people = sum(people)
     starting_index = -1
-    best_remaining_food = float('inf')
+    whole_remaining_food = float('inf')
     for i in range(n):
         remaining_food = prefix_sum[i] - (i + 1) * people[i % n]
         while remaining_food >= 0 and (remaining_food + prefix_sum[n] - (i + 1) * people[(i + 1) % n]) / (total_people - people[i % n]) >= 1:
             remaining_food -= people[i % n]
             remaining_food += food[(i + 1) % n]
             i += 1
-        if i > starting_index and remaining_food < best_remaining_food:
+        if i > starting_index and remaining_food < whole_remaining_food:
             starting_index = i
-            best_remaining_food = remaining_food
+            whole_remaining_food = remaining_food
     if starting_index == -1:
         return -1
     else:
