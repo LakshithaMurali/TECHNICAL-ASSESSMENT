337002f30c438d41ac293bae04868663076e8d7c
Lakshitha M <153210828+LakshithaMurali@users.noreply.github.com>
2023-12-10 14:18:46 +0530

    Update TASK 1(SLIDING WINDOW ).py

--- TASK 1(SLIDING WINDOW ).py
+++ TASK 1(SLIDING WINDOW ).py
@@ -4,18 +4,18 @@ def findBestValue(food, people):
     for i in range(n):
         prefix_sum[i + 1] = prefix_sum[i] + food[i]
     total_people = sum(people)
-    best_starting_index = -1
+    starting_index = -1
     best_remaining_food = float('inf')
     for i in range(n):
-        current_remaining_food = prefix_sum[i] - (i + 1) * people[i % n]
-        while current_remaining_food >= 0 and (current_remaining_food + prefix_sum[n] - (i + 1) * people[(i + 1) % n]) / (total_people - people[i % n]) >= 1:
-            current_remaining_food -= people[i % n]
-            current_remaining_food += food[(i + 1) % n]
+        remaining_food = prefix_sum[i] - (i + 1) * people[i % n]
+        while remaining_food >= 0 and (remaining_food + prefix_sum[n] - (i + 1) * people[(i + 1) % n]) / (total_people - people[i % n]) >= 1:
+            remaining_food -= people[i % n]
+            remaining_food += food[(i + 1) % n]
             i += 1
-        if i > best_starting_index and current_remaining_food < best_remaining_food:
-            best_starting_index = i
-            best_remaining_food = current_remaining_food
-    if best_starting_index == -1:
+        if i > starting_index and remaining_food < best_remaining_food:
+            starting_index = i
+            best_remaining_food = remaining_food
+    if starting_index == -1:
         return -1
     else:
-        return (best_starting_index + 1) % n
+        return (starting_index + 1) % n
